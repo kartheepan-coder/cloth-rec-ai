3e2ccab301602d1fb759a9deb3b966b374a01874
Kartheepan <karthi.thilax@lyfcard.io>
2024-08-17 23:03:23 +0530

    "first "

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App, { Example } from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const Index = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<App />}></Route>
+        <Route path="/category" element={<Example />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>
 );
 
